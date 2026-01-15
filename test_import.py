6934ccfa9a480baa7d26e8c523411d79b5f4bee1
ge57fix <ge57fixytum.de>
2026-01-15 15:11:39 +0100

    Small python file to test the local installation.

--- test_import.py
+++ test_import.py
@@ -1,6 +1,10 @@
 from eradist import ERADist
 import matplotlib.pyplot as plt
 
+# Copy this file into a different directory, in order to test the installed local package. I say different directory,
+# because that way you know that the functions are working not because they are in the same directory, but because
+# the package has been added to the package list used by your python interpreter...
+
 dist = ERADist("Normal","MOM",[0,4])
 ns = 10000
 xs = dist.random(ns)
